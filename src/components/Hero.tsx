5ab335b4d22db6b19169bfc2d8bb7e53e154a9c5
Your Name <gabrielgasparotto45@gmail.com>
2025-07-21 13:38:50 -0300

    style/fix(Navbar/Hero) Resolvendo problema de sobreposição

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,12 +2,12 @@ import backgroundImageUrl from '../assets/torneio-img.png';
 
 function Hero() {
     return (
-        <section className="py-16 bg-white pt-20">
+        <section className="bg-white mt-[80px]">
             <div
                 className="relative max-w-5xl mx-auto rounded-xl overflow-hidden min-h-[550px] flex items-center justify-center bg-cover bg-center"
                 style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,1)), url(${backgroundImageUrl})` }}
             >
-                <div className="text-center text-white drop-shadow-lg z-10 px-4 pt-40 pb-16 w-full ">
+                <div className="text-center text-white drop-shadow-lg z-0 px-4 pt-24 pb-16 w-full">
                     <h1 className="text-5xl font-bold mb-4">Viva o melhor do Esporte na Areia</h1>
                     <p className="text-lg mb-8">
                         Treine, jogue e se divirta nas melhores quadras de areia da região, aproveitando
