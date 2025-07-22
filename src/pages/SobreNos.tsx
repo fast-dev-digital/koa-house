aa9dd65ba267a2c6c169fe03fa51cfdc7d42c869
Your Name <gabrielgasparotto45@gmail.com>
2025-07-22 13:02:02 -0300

    feat(SobreNos) Adicionando page Sobre nos

---
+++ src/pages/SobreNos.tsx
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+import WhatsappFloat from '../components/WhatsappFloat';
+
+
+function SobreNos() {
+    return (
+        <div>
+            
+            <h1>Sobre Nós</h1>
+            
+            <WhatsappFloat />
+        </div>
+    );
+}
+
+export default SobreNos;
