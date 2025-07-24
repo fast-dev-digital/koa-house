ee7bd8324d57d6e2a8e59faf596c8df1290278d5
Your Name <gabrielgasparotto45@gmail.com>
2025-07-23 21:17:58 -0300

    style(Professores) Arrumando divs

--- src/pages/Professores.tsx
+++ src/pages/Professores.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import WhatsappFloat from "../components/WhatsappFloat";
+import ProfessoresSection from "../components/ProfessoresSection";
 
 function Professores() {
     return (
         <div className="pt-20 px-4 md:px-10 relative mt-10">
-            <div className="bg-green-500 text-white rounded-xl p-4 inline-block mb-5 shadow-md">
-                <h1 className='text-2xl font-bold'>Nossos Professores</h1>
+            <div className='bg-green-500 text-white rounded-xl p-4 inline-block mb-5 shadow-md'>
+                <h2 className='text-2xl font-bold'>Nosso time de professores</h2>
             </div>
-            
+            <ProfessoresSection />
             <WhatsappFloat />
         </div>
     );
