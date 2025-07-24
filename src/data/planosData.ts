a8a166a65fd462f2a96dad7ad38d89a56a3f0267
Victor Benatti <victorbenatti13@gmail.com>
2025-07-24 16:41:59 -0300

    feat: Implementa prop de unidade nos dados dos planos, adiciona Locacao como um tipo do type Plano e implementa os novos objetos dos planos

--- src/data/planosData.ts
+++ src/data/planosData.ts
@@ -2,9 +2,10 @@
 
 // Definimos o "formato" do plano
 export type Plano = {
-    tipo: 'Futevôlei' | 'Beach Tennis';
+    tipo: 'Futevôlei' | 'Beach Tennis' | 'Locação';
     titulo: string;
     preco: string;
+    unidade?: string;
     destacado: boolean
     features: string[];
 };
@@ -14,6 +15,7 @@ export const planos: Plano[] = [
         tipo: 'Futevôlei',
         titulo: 'Futevôlei 2x - Semestral',
         preco: 'R$ 199,90',
+        unidade: '/por mês',
         destacado: true, // Este será nosso card em destaque
         features: [
             '2x Aulas de Futevôlei Semanais',
@@ -26,6 +28,7 @@ export const planos: Plano[] = [
         tipo: 'Futevôlei',
         titulo: 'Futevôlei 2x - Trimestral',
         preco: 'R$ 229,90',
+        unidade: '/por mês',
         destacado: false,
         features: [
             '2x Aula de Futevôlei Semanal',
@@ -38,6 +41,7 @@ export const planos: Plano[] = [
         tipo: 'Futevôlei',
         titulo: 'Futevôlei 2x - Mensal',
         preco: 'R$ 249,90',
+        unidade: '/por mês',
         destacado: false,
         features: [
             '2x Aula de Futevôlei Semanal',
@@ -50,6 +54,7 @@ export const planos: Plano[] = [
         tipo: 'Beach Tennis',
         titulo: 'Beach Tennis 2x - Semestral',
         preco: 'R$ 399,90',
+        unidade: '/por mês',
         destacado: true,
         features: [
             '2x Aulas de Beach Tennis Semanais',
@@ -62,6 +67,7 @@ export const planos: Plano[] = [
         tipo: 'Beach Tennis',
         titulo: 'Beach Tennis 2x - Trimestral',
         preco: 'R$ 429,90',
+        unidade: '/por mês',
         destacado: false,
         features: [
             '2x Aula de Beach Tennis Semanal',
@@ -74,12 +80,48 @@ export const planos: Plano[] = [
         tipo: 'Beach Tennis',
         titulo: 'Beach Tennis 2x - Mensal',
         preco: 'R$ 449,90',
+        unidade: '/por mês',
         destacado: false,
         features: [
             '2x Aula de Beach Tennis Semanal',
             'Sem Taxa de Matrícula',
             'Descontos em Serviços',
             'Pagamento Recorrente',
+        ],
+    },
+    {
+        tipo: 'Locação',
+        titulo: 'Locação de Quadra (1 hora) + Churrasqueira',
+        preco: 'R$ 129,90',
+        unidade: '/por hora',
+        destacado: true,
+        features: [
+            '1 Hora de Quadra Exclusiva',
+            'Espaço Gourmet Exclusivo',
+            'Acesso à Estrutura da Arena',
+            'Bolas Inclusas',
+        ],
+    },
+    {
+        tipo: 'Locação',
+        titulo: 'Locação de Quadra',
+        preco: 'R$ 99,90',
+        unidade: '/por hora',
+        destacado: false,
+        features: [
+            '1 Hora de Quadra Exclusiva',
+            'Acesso à Estrutura da Arena',
+            'Bolas Inclusas',
+        ],
+    },
+    {
+        tipo: 'Locação',
+        titulo: 'Locação Churrasqueira',
+        preco: 'R$ 29,90',
+        unidade: '/por hora',
+        destacado: false,
+        features: [
+            'Espaço Gourmet Exclusivo',
         ],
     }
 ]
