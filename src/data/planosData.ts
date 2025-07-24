e626f062f62e3ba2b7f9df95ad802b8f352bb7cf
Victor Benatti <victorbenatti13@gmail.com>
2025-07-24 16:13:36 -0300

    feat: Implementa terceiro card para futevolei e beach tennis dos planos mensais

--- src/data/planosData.ts
+++ src/data/planosData.ts
@@ -35,6 +35,18 @@ export const planos: Plano[] = [
         ],
     },
     {
+        tipo: 'Futevôlei',
+        titulo: 'Futevôlei 2x - Mensal',
+        preco: 'R$ 249,90',
+        destacado: false,
+        features: [
+            '2x Aula de Futevôlei Semanal',
+            'Sem Taxa de Matrícula',
+            'Desconto no Day Use',
+            'Pagamento Recorrente',
+        ],
+    },
+    {
         tipo: 'Beach Tennis',
         titulo: 'Beach Tennis 2x - Semestral',
         preco: 'R$ 399,90',
@@ -50,6 +62,18 @@ export const planos: Plano[] = [
         tipo: 'Beach Tennis',
         titulo: 'Beach Tennis 2x - Trimestral',
         preco: 'R$ 429,90',
+        destacado: false,
+        features: [
+            '2x Aula de Beach Tennis Semanal',
+            'Sem Taxa de Matrícula',
+            'Descontos em Serviços',
+            'Pagamento Recorrente',
+        ],
+    },
+    {
+        tipo: 'Beach Tennis',
+        titulo: 'Beach Tennis 2x - Mensal',
+        preco: 'R$ 449,90',
         destacado: false,
         features: [
             '2x Aula de Beach Tennis Semanal',
