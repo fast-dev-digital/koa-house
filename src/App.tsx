0ad8ff9739763864ee6fe27c2b7178f08d77c1f0
Victor Benatti <victorbenatti13@gmail.com>
2025-07-30 17:11:23 -0300

    feat: Implementa rotas para dashboard do Aluno

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+// src/App.tsx
+
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import AdminLayout from './components/AdminLayout';
@@ -13,6 +15,12 @@ import CadastrarAdmin from './pages/CadastrarAdmin';
 import RedefinirSenha from './pages/ConfirmarSenha';
 import EsqueciSenha from './pages/EsqueciSenha';
 
+// Imports Aluno
+import DashboardAluno from './pages/pagesAluno/DashboardAluno';
+import MinhasTurmas from './pages/pagesAluno/MinhasTurmas';
+import MeusPagamentos from './pages/pagesAluno/MeusPagamentos';
+import MeuPerfil from './pages/pagesAluno/MeuPerfil';
+
 function App() {
   return (
     <Routes>
@@ -26,9 +34,25 @@ function App() {
         <Route path="planos" element={<Planos />} />
         <Route path="redefinir-senha" element={<RedefinirSenha />} />
         <Route path='/esqueci-senha' element={<EsqueciSenha /> } />
-        {/* Rotas de redefinir senha - ambas funcionam */}
-        
+
+        {/* Rotas do Aluno - Protegidas */}
+        <Route
+          path="/aluno/*"
+          element={
+            <ProtectedRoute>
+              <Routes>
+                <Route index element={<DashboardAluno />} />
+                <Route path="turmas" element={<MinhasTurmas />} />
+                <Route path="pagamentos" element={<MeusPagamentos />} />
+                <Route path="perfil" element={<MeuPerfil />} />
+              </Routes>
+            </ProtectedRoute>
+          }
+        />
+        {/* Rotas de redefinir senha - ambas funcionam */}    
       </Route>
+
+
       
       {/* Rota de redefinição FORA do Layout para URLs do Firebase */}
       <Route path="/login/redefinir-senha" element={<RedefinirSenha />} />
